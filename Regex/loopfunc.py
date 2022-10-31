dddd5e43e1ecf1ae25be142ee57cb95a3ca5128e
rohitcrft79 <114180116+rohitcrft79@users.noreply.github.com>
2022-10-31 18:49:01 -0400

    Add files via upload
    This is a code for extracting TC holder and engine information from all the TCDS using data structures/Loop functions.

---
+++ Regex/loopfunc.py
@@ -0,0 +1,44 @@
+from email.policy import strict
+import os
+import pypdf2
+path = "" #folder with all PDFs
+
+list_files = os.listdir(path)
+print(list_files)
+s = ""
+
+for ele in list_files:
+    #print(ele)
+    os.chdir(path)
+ 
+    pdfFileObj = open(ele, 'rb' ,) 
+        
+    #creating a pdf reader object 
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+
+        
+    # printing number of pages in pdf file 
+    page_count = pdfReader.numPages
+    #print(page_count)
+
+
+    for i in range(0,page_count):
+        pageObj = pdfReader.getPage(i)
+        res = pageObj.extractText()
+        out = res.splitlines()
+        for j in range(0,len(out)):
+            if out[j].find('Type Certificate Holder') != -1:
+                tmp = out[j].replace("Type Certificate Holder" , "")
+                new_str = tmp.replace("(See Note X)" , "")
+              
+                print("Type Certificate Holder:Name" + "     " + new_str)
+                print("Type Certificate Holder:Address" + "       " + out[j+1] + out[j+2])
+           
+            elif out[j].find('Engine ') != -1:
+                tmp = out[j].replace("Engine" , "")
+                print("Engine:" + "     " + tmp)
+  
+
+    print("\n")
+    
+    pdfFileObj.close()
